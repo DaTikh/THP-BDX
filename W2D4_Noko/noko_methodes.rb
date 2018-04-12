604c4cc3df6a36691eb52ab2e8375a8c461e30bc
Baptiste Rogeon <baptiste.rogeon@gmail.com>
2018-04-12 14:44:53 +0200

    avec camelCase !!

--- W2D4_Noko/noko_methodes.rb
+++ W2D4_Noko/noko_methodes.rb
@@ -20,10 +20,23 @@ def get_the_email_of_a_townhal_from_its_webpage(url)
   url.each do |url|
     page = Nokogiri::HTML(open(url))
     mairie = page.css("/html/body/div/main/section[1]/div/div/div/h1").text.gsub(" - ","").gsub!(/[0-9]/, "")
+    mairie = camel_name(mairie)
     mairie_mail = page.css("/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]").text
     mairie_info[mairie] = mairie_mail
   end
    return mairie_info
+end
+
+def camel_name(a)
+  if a.include?("-")
+    b = a.downcase!.split("-")
+    b[1..-1].each do |b|
+    b = b.capitalize!
+    end
+  return b.join("")
+  else
+  return a.downcase!
+end
 end
 
 def perform()
