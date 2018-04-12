2572683a1174bfe9a942382005ee9f803c5d3a7b
Baptiste Rogeon <baptiste.rogeon@gmail.com>
2018-04-12 20:47:39 +0200

    test max

--- W2D4_Noko/noko_incubateur.rb
+++ W2D4_Noko/noko_incubateur.rb
@@ -3,4 +3,12 @@ require 'nokogiri'
 require 'open-uri'
 require 'yaml'
 
-page = Nokogiri::HTML(open("http://www.mon-incubateur.com/site_incubateur/incubateurs"))
+# def get_incubator_link()
+page = Nokogiri::HTML(open("http://www.alloweb.org/annuaire-startups/annuaire-incubateurs-startups/wai-paris-we-are-innovation-incubateur-bnp-paribas/"))
+link = page.css("div.wpb_wrapper p a")
+
+ puts link
+
+# end
+#
+# get_incubator_link()
